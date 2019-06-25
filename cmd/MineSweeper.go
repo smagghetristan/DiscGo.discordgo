5cc4c77fc91c6b0b3d0e83bbc6d49236658910db
smagghetristan <31407909+smagghetristan@users.noreply.github.com>
2019-06-25 13:16:31 +0200

    Update MineSweeper.go
    Added comments

--- cmd/MineSweeper.go
+++ cmd/MineSweeper.go
@@ -7,10 +7,12 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+//Will calculate the score of a square
 func ScoreCalc(Field [10][10]string, i int, j int) int {
 	Score := 0
 	for k := i - 1; k < i+2; k++ {
 		for l := j - 1; l < j+2; l++ {
+			//Prevents any glitch on the corners and on the firsts and lasts columns / lines
 			if k >= 0 && l >= 0 {
 				if k <= 9 && l <= 9 {
 					if Field[k][l] == "x" {
@@ -23,7 +25,9 @@ func ScoreCalc(Field [10][10]string, i int, j int) int {
 	return Score
 }
 
+//Function is called when the user types the command
 func MineSweeperCreate(session *discordgo.Session, m *discordgo.MessageCreate) {
+	//Will be used to convert an int to a text for discord
 	Numbers := []string{
 		"||:zero:||",
 		"||:one:||",
@@ -36,6 +40,7 @@ func MineSweeperCreate(session *discordgo.Session, m *discordgo.MessageCreate) {
 		"||:eight:||",
 	}
 	var Field [10][10]string
+	//Generate the bombs, 8/100 chances
 	for i := 0; i < 10; i++ {
 		for j := 0; j < 10; j++ {
 			dice := rand.Intn(100)
@@ -46,6 +51,7 @@ func MineSweeperCreate(session *discordgo.Session, m *discordgo.MessageCreate) {
 			}
 		}
 	}
+	//Calculates the scores of each squares and stores them
 	for i := 0; i < 10; i++ {
 		for j := 0; j < 10; j++ {
 			if Field[i][j] == "." {
@@ -54,6 +60,7 @@ func MineSweeperCreate(session *discordgo.Session, m *discordgo.MessageCreate) {
 		}
 	}
 	Message := ""
+	//Prepare the message and sends it afterwhile
 	for i := 0; i < 10; i++ {
 		for j := 0; j < 10; j++ {
 			if Field[i][j] != "x" {
